b0d8411b7e43b36ce8474f14dc5283010dec3787
Vladyslav Launikonis <vladyslav.launikonis@gmail.com>
2020-06-17 16:10:59 +0300

    17.06.2020 16:10 Create the class of User

---
+++ ConsoleCalculatorOfCalories/ConsoleCalculatorOfCalories.CMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCalculatorOfCalories.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
